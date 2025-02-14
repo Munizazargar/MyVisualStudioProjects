a320afc4f30d0920b16006737b3c9374f146fdee
munizaa.zargar <munizahzargar.iimun@gmail.com>
2025-02-14 22:14:18 +0530

    Add files via upload

---
+++ ACCOUNTS/ACCOUNT.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACCOUNTS
+{
+    public class ACCOUNT<T>
+    {
+        public int amt;
+        private T acc_no;
+        private T acc_type;
+        public void set_acc_no(T acc_no)
+        {
+            this.acc_no = acc_no;
+        }
+        public T get_acc_no()
+        {
+            return acc_no;
+        }
+        public void set_acc_type(T acc_type)
+        {
+            this.acc_type = acc_type;
+        }
+        public T get_acc_type()
+        {
+            return acc_type;
+        }
+        public void deposit<T>()
+        {
+            Console.WriteLine("ENTER AMOUNT");
+            T amt = T (typeof)(Console.ReadLine());   
+            //typeof data type
+        }
+        private T acc_name;
+        public T Acc_name
+        {
+            set { acc_name = value; }
+            get { return acc_name; }
+        }
+        private T balance;
+        public T Balance
+        {
+            set { balance = value; }
+            get { return balance; }
+        }
+        public void confirmation()
+        {
+            Console.WriteLine("THE " + acc_type + " BANK ACCOUNT OF " + acc_name + " HAVING ACCOUNT NO = " + acc_no + " IS HAVING A TOTAL BALANCE OF = " + balance + " AFTER THE WITHDRAWAL OF RS = " + amt);
+        }
+
+    }
+}
---
+++ ACCOUNTS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACCOUNTS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ACCOUNT <string> a = new ACCOUNT<string>();
+            a.set_acc_no("account no = 1235");
+            a.set_acc_type("account type = savings");
+            Console.WriteLine(a.get_acc_no());
+            Console.WriteLine(a.get_acc_type());
+            Console.WriteLine(a.Acc_name = "account name = laveezah");
+            Console.WriteLine(a.Balance = "account balance = 3000");
+            ACCOUNT<int> b = new ACCOUNT<int>();
+            
+
+            a.confirmation();
+            
+            Console.ReadLine();
+        }
+    }
+}
