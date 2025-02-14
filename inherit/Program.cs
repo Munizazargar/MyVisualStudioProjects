460916e6ad9140c662d1468135f0cdb46f33c94a
munizaa.zargar <munizahzargar.iimun@gmail.com>
2025-02-14 22:32:49 +0530

    Add files via upload

---
+++ inherit/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inherit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Console.WriteLine(employee.Exp = 3);
+            Console.WriteLine(employee.Amount = 10000);
+            Console.WriteLine(employee.EmployeeName = "muniza");
+            Console.WriteLine( employee.Employee_ID = 8339);
+            employee.salary();
+
+            Console.ReadLine();
+        }
+
+    }
+    public class manager
+    {
+        private int employee_id;
+            public int Employee_ID
+            {
+            set { employee_id = value; }
+            get { return employee_id; }
+            }
+        private string employee_name;
+            public string EmployeeName
+            {
+            set { employee_name = value; }
+            get { return employee_name; }
+            }
+
+
+    }
+}
---
+++ inherit/employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inherit
+{
+
+    public class Employee : manager
+    {
+        static private int exp;
+        public int Exp
+        {
+            set { exp = value; }
+            get { return exp; }
+        }
+        static private int amount;
+        public int Amount
+        {
+            set { amount = value; }
+            get { return amount; }
+        }
+        public void salary()
+        {
+            
+            exp = 3;
+            amount = 10000;
+            int salary = Exp * amount;
+            Console.WriteLine( salary);
+         
+
+        }
+        
+
+
+    }
+
+    
+
+}
