30f04a4c0ec33ac4a4a2e95ec40305f02059926f
munizaa.zargar <munizahzargar.iimun@gmail.com>
2025-02-14 22:03:50 +0530

    Add files via upload

---
+++ CREDITCARD/ACCOUNT.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CREDITCARD
+{
+    public class ACCOUNT
+    {
+        private string accountname;
+        private int accountno;
+        private int amount;
+        public void setaccname(string accountname)
+        {
+            this.accountname = accountname;
+        }
+        public string getaccname()
+        {
+            return this.accountname;
+        }
+        public void setaccountno(int accountno)
+        {
+            this.accountno = accountno;
+        }
+        public int getaccountno()
+        {
+            return this.accountno;
+        }
+        public void setamount(int amount)
+        {
+            this.amount = amount;
+        }
+        public int getamount()
+        {
+            return this.amount;
+        }
+    }
+}
---
+++ CREDITCARD/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CREDITCARD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           UPI pi = new UPI();
+           pi.infoacccrd();
+            
+           
+            Console.ReadLine();
+        }
+    }
+}
